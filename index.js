35b1f23e6d6355bf41232f491dcba98ee68f5896
Jakub Sedlacek <sedlacek.jakub@gmail.com>
2018-12-19 11:39:29 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello world!');
+});
+
+const port = process.env.PORT || 1234;
+
+app.listen(port, () => {
+  console.log(`listening on ${port}`);
+});
